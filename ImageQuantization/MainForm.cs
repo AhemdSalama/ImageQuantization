dd75938c65a5f3ddb30963872b72e9c241168465
Momen <zaaza1233@gmail.com>
2019-12-14 01:46:26 +0200

    bullshit changes

--- ImageQuantization/MainForm.cs
+++ ImageQuantization/MainForm.cs
@@ -1,9 +1,5 @@
 using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
-using System.Windows.Forms;
-=======
->>>>>>> primAlgo
 using System.Diagnostics;
 using System.Windows.Forms;
 
@@ -15,38 +11,6 @@ namespace ImageQuantization
         {
             InitializeComponent();
         }
-<<<<<<< HEAD
-        public struct rgbPixel
-        {
-            public int red, green, blue;
-            public rgbPixel(int red , int green , int blue)
-            {
-                this.red = red;
-                this.green = green;
-                this.blue = blue;
-            }
-        }
-
-        RGBPixel[,] ImageMatrix;
-        SortedSet<int> uniqeColors = new SortedSet<int>();
-        List<Edge> edges = new List<Edge>();
-        List<RGBPixel> ucolors = new List<RGBPixel>();
-        int height;
-        int width;
-        public static rgbPixel IntToRGB (int value)
-        {
-            var red = (value >> 0) & 255;
-            var green = (value >> 8) & 255;
-            var blue = (value >>16) & 255 ;
-            return new rgbPixel(red, green, blue);
-
-        }
-        public int RGBToInt (int r , int g , int b)
-        {
-            return (r << 0) | (g << 8) | (b << 16);
-        }
-=======
->>>>>>> primAlgo
 
         RGBPixel[,] ImageMatrix;        // our picture
         private void btnOpen_Click(object sender, EventArgs e)
@@ -72,40 +36,8 @@ namespace ImageQuantization
         }
 
         // Return the function with the smallest cost
-        
 
-<<<<<<< HEAD
-            //TIME : O(H*W)
-            //SPACE : O(D^2)
-            for (int i = 0; i < height;i++ )
-            for (int j = 0; j < width; j++)
-            {
-                int value = RGBToInt(ImageMatrix[i, j].red, ImageMatrix[i, j].green, ImageMatrix[i, j].blue);
-                uniqeColors.Add(value);
-            }
 
-            //TIME: Exact SIZE OF HASHSET
-            foreach (var c in uniqeColors)
-            {
-                rgbPixel rgb = IntToRGB(c);
-                RGBPixel rgbcolor = new RGBPixel();
-                rgbcolor.red = (byte)(rgb.red);
-                rgbcolor.green = (byte)(rgb.green);
-                rgbcolor.blue = (byte)(rgb.blue);
-
-                ucolors.Add(rgbcolor);
-            }
-           
-            // STEP[2] Get Distance between each color 
-            Edge edge ;
-            double cost=0;
-            double sum = 0;
-            int dred, dgreen, dblue; // contain deffrence 
-            int size = ucolors.Count;
-
-            //TIME O(SIZE^2)
-            for (int i = 0; i < size; i++)
-=======
 
         private void btnQ_Click(object sender, EventArgs e)
         {
@@ -137,7 +69,6 @@ namespace ImageQuantization
             }
 
             foreach (var uniqColor in uniqColors)
->>>>>>> primAlgo
             {
                 var color = RgbPixel.IntToRGB(uniqColor);
                 uColors.Add(color);
